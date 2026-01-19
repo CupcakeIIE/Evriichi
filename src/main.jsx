2fbe22352d49ae71fd1f71a28673b6b893f82ff2
CupcakeIIE <cupcake.ensiie@gmail.com>
2026-01-19 15:01:31 +0100

    new test

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // optionnel, si tu as du CSS
+// import "./index.css"; // optionnel, si tu as du CSS
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
